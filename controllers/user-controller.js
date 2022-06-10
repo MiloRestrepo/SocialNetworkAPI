b5c07c590b37618b3bfddde6b855c8b8ab0ce55a
Troy <camilo1997@yahoo.com>
2022-06-10 02:12:42 -0400

    finished

---
+++ controllers/user-controller.js
@@ -0,0 +1,109 @@
+// import the User model
+const { User } = require('../models');
+
+// Create controller which holds the functionality BEFORE creating the API routes
+// Below are the five main CRUD methods for the /api/user endpoint
+// and two additional methods for adding/removing a friend
+const userController = {
+    // get all users, i.e. GET /api/users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // get one user by id, i.e. GET /api/users/:id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'friends',
+                populate: { path: 'friends' },
+                select: '-__v'
+            })
+            .populate({
+                path: 'thoughts'
+            })
+            .then(dbUserData => {
+                // If no user is found, send 404
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // create a user, i.e. POST /api/users
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+    // update user by id, i.e. PUT /api/users/:id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // delete user, i.e. DELETE /api/users/:id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // add a friend to the user's friend list, i.e. POST /api/users/:userId/friends/:friendId
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Sorry. User id or friend id is incorrect.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Sorry. User id or friend id is incorrect.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thought-controller');
+
+// Set up GET all and POST at /api/thoughts
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(createThought);
+
+// GET one, PUT, and DELETE one thought /api/thoughts/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// POST new reaction to thoughts /api/thoughts/:thoughtId/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
+
+// DELETE reaction to thoughts /api/thoughts/:thoughtId/reactions/:reactionId
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
